032b6baa9dabe90e61c40aa95fe6c7823171f53e
Guilherme B <63854432+gbguimaraes@users.noreply.github.com>
2021-02-15 18:03:54 -0300

    knowledge

---
+++ knowledge/versao-inicial/backend/knexfile.js
@@ -0,0 +1,17 @@
+const { db } = require('./.env')
+
+module.exports = {
+    client: 'postgresql',
+    connection: {
+        database: 'knowledge',
+        user: 'postgres',
+        password: '123456789'
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+    migrations: {
+        tableName: 'knex_migrations'
+    }
+};
